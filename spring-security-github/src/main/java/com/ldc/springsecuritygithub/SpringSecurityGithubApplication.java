57cbe4164358646de35ae744d85233b85d5eb76b
ldcaws <568702182@qq.com>
2020-04-24 23:49:44 +0800

    spring-security添加github第三方登录测试模块相关

---
+++ spring-security-github/src/main/java/com/ldc/springsecuritygithub/SpringSecurityGithubApplication.java
@@ -0,0 +1,13 @@
+package com.ldc.springsecuritygithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityGithubApplication.class, args);
+    }
+
+}
---
+++ spring-security-github/src/test/java/com/ldc/springsecuritygithub/SpringSecurityGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldc.springsecuritygithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
