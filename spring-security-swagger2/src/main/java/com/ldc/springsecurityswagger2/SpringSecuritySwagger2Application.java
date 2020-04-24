a1d2330a514aac5e26d04353fa439ee95016e8b8
ldcaws <568702182@qq.com>
2020-04-24 22:51:30 +0800

    spring-security添加swagger2的相关文件

---
+++ spring-security-swagger2/src/main/java/com/ldc/springsecurityswagger2/SpringSecuritySwagger2Application.java
@@ -0,0 +1,13 @@
+package com.ldc.springsecurityswagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySwagger2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySwagger2Application.class, args);
+    }
+
+}
---
+++ spring-security-swagger2/src/test/java/com/ldc/springsecurityswagger2/SpringSecuritySwagger2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldc.springsecurityswagger2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySwagger2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
