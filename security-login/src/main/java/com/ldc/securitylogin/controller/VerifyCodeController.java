13ea74340e88dad9ba3f2cf31dc74b2dbebb5768
ldcaws <568702182@qq.com>
2020-05-26 09:24:10 +0800

    spring-security添加security-login模块

---
+++ security-login/src/main/java/com/ldc/securitylogin/controller/VerifyCodeController.java
@@ -0,0 +1,36 @@
+package com.ldc.securitylogin.controller;
+
+import com.google.code.kaptcha.Producer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * @description:
+ * @author: ss
+ * @time: 2020/5/25 16:31
+ */
+@RestController
+public class VerifyCodeController {
+
+    @Resource
+    Producer producer;
+
+    @GetMapping("/vc.jpg")
+    public void getVerifyCode(HttpServletResponse resp, HttpSession session) throws IOException {
+        resp.setContentType("image/jpeg");
+        String text = producer.createText();
+        session.setAttribute("verify_code", text);
+        BufferedImage image = producer.createImage(text);
+        try(ServletOutputStream out = resp.getOutputStream()) {
+            ImageIO.write(image, "jpg", out);
+        }
+    }
+}
