654f1b6edccb1d3d9790e26de4ebcd5899a02579
ldcaws <568702182@qq.com>
2020-06-23 21:31:33 +0800

    spring-security添加雪花算法生成UUID模块

---
+++ snowdemo/src/main/java/com/ldc/snowdemo/SnowdemoApplication.java
@@ -0,0 +1,13 @@
+package com.ldc.snowdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowdemoApplication.class, args);
+    }
+
+}
---
+++ snowdemo/src/test/java/com/ldc/snowdemo/SnowdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldc.snowdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
